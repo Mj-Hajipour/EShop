e29ba4fd881137e07a4c9646b5a72bd96214b059
Pigarso.ir <Mj.hajipour19@gmail.com>
2024-10-22 08:53:40 +0330

    EShop E34 => End

---
+++ EShop_account/form.py
@@ -0,0 +1,13 @@
+from django import forms
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class':'form-control','placeholder':'لطفا نام کاربری خود را وارد کنید'}),
+        label='نام کاربری'
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder':'لطفا کلمه عبور خود را وارد نمایید'}),
+        label='کلمه ی عبور'
+    )
--- EShop_account/views.py
+++ EShop_account/views.py
@@ -1,13 +1,31 @@
-from lib2to3.fixes.fix_input import context
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from EShop_account.form import LoginForm
+from django.contrib.auth import authenticate, login, logout,get_user_model
 
 # Create your views here.
 
 
-def Login(request):
-    context={}
+def Login_user(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    login_form = LoginForm(request.POST or None)
+    if login_form.is_valid():
+        username = login_form.cleaned_data['username']
+        password = login_form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return  redirect('/admin')
+
+    context={
+        'login_form':login_form
+    }
     return  render(request,'account/Login.html',context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
 def register(request):
     context={}
